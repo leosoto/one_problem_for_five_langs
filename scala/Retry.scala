39a6c6022cc73c68b00a8528640d2a3cbc8e1048
Leo Soto M <leo.soto@gmail.com>
2010-08-04 18:59:05 -0400

    Scala implementation

---
+++ scala/Retry.scala
@@ -0,0 +1,40 @@
+import java.lang.Math;
+
+class GuateoException extends Exception;
+
+object Retry {
+  def servicioPenca {
+    println("Servicio invocado");
+    if (Math.random < 0.8) {
+      throw new GuateoException
+    }
+    println("Servicio ejecutado exitosamente");
+  }
+
+  def solucion {
+    retry(times=10, when=classOf[GuateoException], wait=2000) {
+      servicioPenca
+    }
+  }
+
+  def retry[R, E <: Exception](times: Int, when: java.lang.Class[E], wait: Int)(body: => R): R = {
+    var lastException:Throwable = null;
+    for (i <- 0 until times) {
+      try {
+	return body
+      } catch {
+	case e  => {
+	  if (when isAssignableFrom e.getClass()) {
+	    lastException = e
+	    Thread.sleep(wait)
+	  } else {
+	    throw e
+	  }
+	}
+      }
+    }
+    throw lastException;
+  }
+
+  def main(args: Array[String]) = solucion;
+}
